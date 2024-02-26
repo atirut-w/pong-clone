ce0201805694bcab94c5eee5e5f73e57ab5b6830
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-02-26 20:48:26 +0700

    Display window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
+#include <raylib.h>
+
 int main()
 {
+    InitWindow(640, 480, "Pong Clone");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(GRAY);
+        EndDrawing();
+    }
+    
     return 0;
 }
